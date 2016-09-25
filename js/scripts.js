e45384f494bf775f6a97962982ae979d1a79aa48
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2016-09-25 11:13:39 -0700

    edit javascript in an attempt to create functionality for the site.

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,47 @@
 $(document).ready(function() {
-
+$("form").submit(function(){
+  event.preventDefault();
   $("submit#button").click(function(event) {
-    var sportInput = ($('#sport').val());
-    var foodInput = ($('#food').val());
-    var weatherInput = ($('#weather').val());
-    var flowerInput = ($('#flower').val());
-    var houseInput = ($('#house').val());
-  })
-})
+    var sport = ($('#sport').val());
+    var food = ($('#food').val());
+    var weather = ($('#weather').val());
+    var flower = ($('#flower').val());
+    var house = ($('#house').val());
+    var answer;
 
-    event.preventDefault();
-
-    if (sportInput) {
-      if (sportInput === "tennis") {
-      } else if ( sportInput === "Basketball" && foodInput === "Grilled Cheese" ) {
-        if (weatherInput === "Rainy") {
-          if (flowerInput === "Rose") {
-            if (houseInput === "Houseboat") {
-              $("#results").addClass(".Olympic");
-              $("#results").show();
-
-            } else if (houseInput === "Lakehouse") {
-              $("#results").addClass(".Yosemite");
-              $("#results").show();
-
-            } else if (houseInput === "Cabin in the Woods" ) {
-            $("#results").addClass(".Glacier");
-            $("#results").show();
-
+    if (sport === "Tennis") {
+      if (food === "Grilled Cheese") {
+        if (weather === "Rainy") {
+          if (flower === "Rose") {
+            if (house === "Houseboat") {
+            answer = "Olympic";
+          } else if (sport === "Tennis"){
+            if (food === "Pizza") {
+              if (weather === "Rainy") {
+                if (flower === "Rose") {
+                  if (house === "Houseboat") {
+                  answer = "Olympic";
+                } else if (sport === "Tennis") {
+                  if (food === "Cheeseburger") {
+                    if (weather === "Rainy") {
+                      if (flower === "Rose") {
+                        if (house === "Houseboat") {
+                        answer = "Olympic";
+                      }
+                    }
+                    }
+                  }
+                }
+              }
+              }
             }
-
-    } else if (weatherInput === "Sunny") {
-      if (flowerInput === "Daffodil") {
-        if (houseInput === "Cabin in the Woods"){
-          $("#results").addClass(".Glacier");
-          $("#results").show();
+          }
         }
-      }
-    }
-  } else if (weatherInput === 'Winter Wonderland') {
-      if (foodInput === 'Grilled Cheese') {
-        if (houseInput === "Houseboat") {
-          if (flowerInput === "Rose") {
-        $("#results").addClass(".Glacier");
-        $("#results").show();
       }
     }
   }
-      } else if (weatherInput === 'Winter Wonderland') {
-        $("#results").addClass(".Yosemite");
-        $("#results").show();
 
-      } else if (foodInput === "pizza") {
-        $("#results").addClass(".Olympic");
-        $("#results").show();
-      }
-
-
-    } else if (flowerInput === 'Sunflower') {
-        if (houseInput === 'Lake House') {
-          $("#results").addClass(".Yosemite");
-          $("#results").show();
-
-        } else if (sportInput === 'basketball') {
-          $("#results").addClass(".Olympic");
-          $("#results").show();
-
-        } else if (sportInput === 'football') {
-          $("#results").addClass(".Glacier");
-          $("#results").show();
-        }
-      }
+    $("#output").text(answer);
+  });
+});
+});
