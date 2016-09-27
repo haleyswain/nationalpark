909b0071d319da1382ee5b5788e3f6b3e214a9a3
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2016-09-27 09:00:49 -0700

    rearrange js into front end and back end code, create a readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-var result ;
-var park = function (trip, animal){
-
+var park = function(trip, animal){
   if (trip) {
     if (trip ==="ski"){
       if (animal === "panda"){
@@ -10,52 +8,60 @@ var park = function (trip, animal){
       } else if (animal === "kangaroo"){
         return "glacier";
       } else if (animal === "bear"){
-        return "glacier"
+        return "glacier";
       }
 
     } else if (trip ==="beach") {
-        if (animal === "panda"){
+        if (animal === "panda") {
           return "olympic";
-        } else if (animal === "giraffe"){
+        } else if (animal === "giraffe") {
           return "yellowstone";
         } else if (animal === "kangaroo"){
           return "olympic";
         } else if (animal === "bear"){
-          return "glacier"
+          return "glacier";
         }
 
-      } else if (trip === "beach") {
-          if (animal === "panda"){
+
+    } else if (trip === "backcountry") {
+        if (animal === "panda"){
             return "yellowstone";
           } else if (animal === "giraffe"){
-            return "olympic";
-          } else if (animal === "kangaroo"){
             return "yellowstone";
+          } else if (animal === "kangaroo"){
+            return "glacier";
           } else if (animal === "bear"){
-            return "glacier"
-          }
-
-        } else if (trip === "backcountry") {
-            if (animal === "panda"){
-              return "yellowstone";
-            } else if (animal === "giraffe"){
-              return "yellowstone";
-            } else if (animal === "kangaroo"){
-              return "glacier";
-            } else if (animal === "bear"){
-              return "olympic"
+              return "olympic";
             }
+          }
+        }
+};
 
-      }
+  $(document).ready(function() {
+    $("form#quiz").submit(function(event) {
+      event.preventDefault();
+          alert("hi")
+      var trip = $('select#trip').val();
+      var food = $('select#food').val();
+      var sport = $('select#sport').val();
+      var people = $('select#people').val();
+      var animal = $('select#animal').val();
+      var result = park(trip, animal);
+      console.log(result)
+
+
+
+    if (result === "glacier"){
+      $("#destination").text("Glacier")
+      $("#result").show();
+    } else if (result === "olympic") {
+        $("#destination").text("Olympic")
+        $("#result").show();
+    } else if (result === "yellowstone"){
+        $("#destination").text("Yellowstone")
+        $("#result").show()
     }
-  }
 
-  $(document).ready(function(){
-  $("form").submit(function(event){
 
-    var trip = $('#trip').val();
-    var food = $('#food').val();
-    var sport = $('#sport').val();
-    var people = $('#people').val();
-    var animal = $('#animal').val();
-    var result = park(trip, bear);
+    });
+  });
